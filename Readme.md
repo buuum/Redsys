4f8045fba6f45b9f09fa82e5dc42fdd68e7761ac
Ferran Martinez <alfonsmartinezparra@gmail.com>
2017-03-06 07:56:38 +0100

    redsys

---
+++ Readme.md
@@ -0,0 +1,295 @@
+Simple php class with redsys soap and redirect
+================================
+
+[![Packagist](https://img.shields.io/packagist/v/buuum/redsys.svg)](https://packagist.org/packages/buuum/redsys)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg?maxAge=2592000)](#license)
+
+## Install
+
+### System Requirements
+
+You need PHP >= 5.6.0 to use Buuum\Redsys but the latest stable version of PHP is recommended.
+
+### Composer
+
+Buuum\redsys is available on Packagist and can be installed using Composer:
+
+```
+composer require buuum/redsys
+```
+
+### Manually
+
+You may use your own autoloader as long as it follows PSR-0 or PSR-4 standards. Just put src directory contents in your vendor directory.
+
+
+### Documentation
+
+https://canales.redsys.es/canales/ayuda/documentacion/Manual%20integracion%20para%20conexion%20por%20Redireccion.pdf
+https://canales.redsys.es/canales/ayuda/documentacion/Manual%20integracion%20para%20conexion%20por%20Web%20Service.pdf
+
+
+### Payment with redirection
+#### OPTION 1.- payment.php (number visa in bank web)
+```php
+$redsys = new \Buuum\Redsys($redsys_key);
+
+try {
+    $redsys->setMerchantcode($redsys_merchant_code);
+    $redsys->setAmount($amount);
+    $redsys->setOrder($order);
+    $redsys->setTerminal($redsys_merchant_terminal);
+    $redsys->setCurrency(978);
+
+    $redsys->setTransactiontype('0');
+    $redsys->setMethod('C');
+
+    $redsys->setNotification('http://localhost/notification.php'); //Url de notificacion
+    $redsys->setUrlOk('http://localhost/payment_ok.php');
+    $redsys->setUrlKo('http://localhost/payment_ko.php');
+
+    $redsys->setTradeName('Store S.L');
+    $redsys->setTitular('John Doe');
+    $redsys->setProductDescription('Product description');
+
+    $form = $redsys->createForm();
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die;
+}
+
+echo $form;
+
+```
+
+#### OPTION 2.- payment.php (number visa in web)
+```php
+$redsys = new \Buuum\Redsys($redsys_key);
+
+try {
+    $redsys->setMerchantcode($redsys_merchant_code);
+    $redsys->setAmount($amount);
+    $redsys->setOrder($order);
+    $redsys->setTerminal($redsys_merchant_terminal);
+    $redsys->setCurrency(978);
+
+    $redsys->setPan($visa_number);
+    $redsys->setExpiryDate($visa_expiry);
+    $redsys->setCVV($visa_cvv);
+    $redsys->setMerchantDirectPayment(true);
+
+    $redsys->setTransactiontype('0');
+    $redsys->setMethod('C');
+
+    $redsys->setNotification('http://localhost/notification.php'); //Url de notificacion
+    $redsys->setUrlOk('http://localhost/payment_ok.php');
+    $redsys->setUrlKo('http://localhost/payment_ko.php');
+
+    $redsys->setTradeName('Store S.L');
+    $redsys->setTitular('John Doe');
+    $redsys->setProductDescription('Product description');
+
+    $form = $redsys->createForm();
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die;
+}
+
+echo $form;
+
+```
+
+#### notification.php
+```php
+$redsys = new \Buuum\Redsys($redsys_key);
+
+try{
+    $result = $redsys->checkPaymentResponse($_POST);
+catch (Exception $e) {
+    echo $e->getMessage();
+    die;
+}
+
+var_dump($result);
+```
+#### notification result output
+##### with error
+```php
+array (
+  'error' => true,
+  'code' => 'SIS041',
+  'Ds_Date' => '05/03/2017',
+  'Ds_Hour' => '08:40',
+  'Ds_SecurePayment' => '1',
+  'Ds_Card_Country' => '724',
+  'Ds_Amount' => '1000',
+  'Ds_Currency' => '978',
+  'Ds_Order' => '99699629',
+  'Ds_MerchantCode' => 'xxxxxx',
+  'Ds_Terminal' => '001',
+  'Ds_Response' => '0000',
+  'Ds_MerchantData' => '',
+  'Ds_TransactionType' => '0',
+  'Ds_ConsumerLanguage' => '1',
+  'Ds_AuthorisationCode' => '875284',
+)
+```
+##### without error
+```php
+array (
+  'error' => false,
+  'code' => 0,
+  'Ds_Date' => '05/03/2017',
+  'Ds_Hour' => '08:40',
+  'Ds_SecurePayment' => '1',
+  'Ds_Card_Country' => '724',
+  'Ds_Amount' => '1000',
+  'Ds_Currency' => '978',
+  'Ds_Order' => '99699629',
+  'Ds_MerchantCode' => 'xxxxxxx',
+  'Ds_Terminal' => '001',
+  'Ds_Response' => '0000',
+  'Ds_MerchantData' => '',
+  'Ds_TransactionType' => '0',
+  'Ds_ConsumerLanguage' => '1',
+  'Ds_AuthorisationCode' => '875284',
+)
+```
+
+### Payment with web service
+
+#### Payment
+
+```php
+$redsys = new \Buuum\Redsys($redsys_key);
+
+try {
+    $redsys->setMerchantcode($redsys_merchant_code);
+    $redsys->setAmount($amount);
+    $redsys->setOrder($order);
+    $redsys->setTerminal($redsys_merchant_terminal);
+    $redsys->setCurrency(978);
+    $redsys->setPan($visa_number);
+    $redsys->setExpiryDate($visa_expiry);
+    $redsys->setCVV($visa_cvv);
+    $redsys->setTransactiontype('A');
+    $redsys->setIdentifier('REQUIRED');
+    $result = $redsys->firePayment();
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die;
+}
+
+var_dump($result);
+
+```
+
+#### Payment with identifier
+```php
+
+$redsys = new \Buuum\Redsys($redsys_key);
+
+try {
+    $redsys->setMerchantcode($redsys_merchant_code);
+    $redsys->setAmount($amount);
+    $redsys->setOrder($order);
+    $redsys->setTerminal($redsys_merchant_terminal);
+    $redsys->setCurrency(978);
+    $redsys->setTransactiontype('A');
+    $redsys->setIdentifier($client_identifier);
+    $result = $redsys->firePayment();
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die;
+}
+```
+
+#### Pay back from order
+```php
+
+$redsys = new \Buuum\Redsys($redsys_key);
+
+try {
+    $redsys->setMerchantcode($redsys_merchant_code);
+    $redsys->setAmount($amount);
+    $redsys->setOrder($order);
+    $redsys->setTerminal($redsys_merchant_terminal);
+    $redsys->setCurrency(978);
+    $redsys->setTransactiontype(3);
+    $result = $redsys->firePayment();
+    
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die;
+}
+```
+
+#### result output
+##### with error
+```php
+array (
+  'error' => true,
+  'code' => 'SIS0051',
+  'DS_MERCHANT_MERCHANTCODE' => 'xxxxx',
+  'DS_MERCHANT_AMOUNT' => '1000',
+  'DS_MERCHANT_ORDER' => '9932453',
+  'DS_MERCHANT_TERMINAL' => '001',
+  'DS_MERCHANT_CURRENCY' => '978',
+  'DS_MERCHANT_PAN' => '4548812049400004',
+  'DS_MERCHANT_EXPIRYDATE' => '2012',
+  'DS_MERCHANT_CVV2' => '123',
+  'DS_MERCHANT_TRANSACTIONTYPE' => 'A',
+  'DS_MERCHANT_IDENTIFIER' => 'REQUIRED',
+)
+```
+##### without error
+```php
+array (
+  'error' => false,
+  'code' => '0',
+  'Ds_Amount' => '1000',
+  'Ds_Currency' => '978',
+  'Ds_Order' => '1234524534',
+  'Ds_Signature' => 'Xfh84TG95t7XRKQV/UGyhH+lXd6PFuGPeU25fgNpGUc=',
+  'Ds_MerchantCode' => 'xxxxxx',
+  'Ds_Terminal' => '1',
+  'Ds_Response' => '0000',
+  'Ds_AuthorisationCode' => '415446',
+  'Ds_TransactionType' => 'A',
+  'Ds_SecurePayment' => '0',
+  'Ds_Language' => '1',
+  'Ds_ExpiryDate' => '2012',
+  'Ds_Merchant_Identifier' => '4597a931b735a7d8e55252e25894fa6dd3a9bed4',
+  'Ds_MerchantData' => array (),
+  'Ds_Card_Country' => '724',
+)
+```
+
+### Get errors messages
+For get error messages use class https://github.com/eusonlito/redsys-Messages
+```php
+$error = \Redsys\Messages\Messages::getByCode($result['code']);
+
+# error output
+array (
+  'code' => 'SIS0051',
+  'message' => 'Ds_Merchant_Order Número de pedido repetido',
+  'msg' => 'MSG0001',
+  'detail' => '',
+)
+```
+
+## LICENSE
+
+The MIT License (MIT)
+
+Copyright (c) 2016
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/Redsys/Redsys.php
@@ -0,0 +1,614 @@
+<?php
+
+namespace Buuum;
+
+class Redsys
+{
+
+    /**
+     * @var string
+     */
+    protected $_enviroment = 'https://sis-t.redsys.es:25443/sis/services/SerClsWSEntrada?wsdl';
+
+    /**
+     * @var string
+     */
+    protected $type = 'webservice';
+
+    /**
+     * @var array
+     */
+    protected $_parameters = [];
+
+    /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * Redsys constructor.
+     * @param string $key
+     */
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * Currency in ISO 4217
+     * https://en.wikipedia.org/wiki/ISO_4217
+     *
+     * @param int $currency
+     * @throws \Exception
+     */
+    public function setCurrency($currency = 978)
+    {
+        if (is_numeric($currency)) {
+            $this->_parameters['DS_MERCHANT_CURRENCY'] = $currency;
+        } else {
+            throw new \Exception('Currency is not valid');
+        }
+    }
+
+    /**
+     * (required)
+     *
+     * @param string $value
+     * @throws \Exception
+     */
+    public function setIdentifier($value = 'REQUIRED')
+    {
+        if (strlen(trim($value)) > 0) {
+            $this->_parameters['DS_MERCHANT_IDENTIFIER'] = $value;
+        } else {
+            throw new \Exception('Please add value');
+        }
+    }
+
+    /**
+     * Para Euros las dos últimas posiciones se consideran decimales. (required)
+     *
+     * @param $amount
+     * @throws \Exception
+     */
+    public function setAmount($amount)
+    {
+        if ($amount >= 0) {
+            $amount = $this->convertNumber($amount);
+            $amount = intval(strval($amount * 100));
+            $this->_parameters['DS_MERCHANT_AMOUNT'] = $amount;
+        } else {
+            throw new \Exception('Amount must be greater than equal 0.');
+        }
+    }
+
+    /**
+     * Código FUC asignado al comercio (required)
+     *
+     * @param $fuc
+     * @throws \Exception
+     */
+    public function setMerchantcode($fuc)
+    {
+        if (strlen(trim($fuc)) > 0) {
+            $this->_parameters['DS_MERCHANT_MERCHANTCODE'] = $fuc;
+        } else {
+            throw new \Exception('Please add Fuc');
+        }
+    }
+
+    /**
+     * Payment type
+     * [T = Pago con Tarjeta + iupay , R = Pago por Transferencia, D = Domiciliacion,
+     * C = Sólo Tarjeta (mostrará sólo el formulario para datos de tarjeta)] por defecto es T
+     *
+     * @param string $method
+     * @throws \Exception
+     */
+    public function setMethod($method = 'T')
+    {
+        if (strlen(trim($method)) > 0) {
+            $this->_parameters['DS_MERCHANT_PAYMETHODS'] = trim($method);
+        } else {
+            throw new \Exception('Add pay method');
+        }
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setNotification(string $url = '')
+    {
+        $this->_parameters['DS_MERCHANT_MERCHANTURL'] = $url;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrlOk(string $url = '')
+    {
+        $this->_parameters['DS_MERCHANT_URLOK'] = $url;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrlKo(string $url = '')
+    {
+        $this->_parameters['DS_MERCHANT_URLKO'] = $url;
+    }
+
+    /**
+     * Set Trade name Trade name will be reflected in the ticket trade (Optional)
+     *
+     * @param string $tradename trade name
+     * @throws \Exception
+     */
+    public function setTradeName($tradename = '')
+    {
+        if (strlen(trim($tradename)) > 0) {
+            $this->_parameters['DS_MERCHANT_MERCHANTNAME'] = trim($tradename);
+        } else {
+            throw new \Exception('Add name for Trade name');
+        }
+    }
+
+    /**
+     * Set product description (optional)
+     *
+     * @param string $description
+     * @throws \Exception
+     */
+    public function setProductDescription($description = '')
+    {
+        if (strlen(trim($description)) > 0) {
+            $this->_parameters['DS_MERCHANT_PRODUCTDESCRIPTION'] = trim($description);
+        } else {
+            throw new \Exception('Add product description');
+        }
+    }
+
+    /**
+     * Set name of the user making the purchase (required)
+     *
+     * @param string $titular name of the user (for example Alonso Cotos)
+     * @throws \Exception
+     */
+    public function setTitular($titular = '')
+    {
+        if (strlen(trim($titular)) > 0) {
+            $this->_parameters['DS_MERCHANT_TITULAR'] = trim($titular);
+        } else {
+            throw new \Exception('Add name for the user');
+        }
+    }
+
+    /**
+     *
+     * Los 4 primeros dígitos deben ser numéricos, para los dígitos
+     * restantes solo utilizar los siguientes caracteres ASCII (required)
+     * Del 30 = 0 al 39 = 9
+     * Del 65 = A al 90 = Z
+     * Del 97 = a al 122 = z
+     *
+     * @param $order
+     * @throws \Exception
+     */
+    public function setOrder($order)
+    {
+        if (strlen(trim($order)) > 0) {
+            $this->_parameters['DS_MERCHANT_ORDER'] = $order;
+        } else {
+            throw new \Exception('Add Order');
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrder()
+    {
+        return $this->_parameters['DS_MERCHANT_ORDER'];
+    }
+
+    /**
+     * (required)
+     * Para el comercio para indicar qué tipo de transacción es. Los posibles valores son:
+     * A – Pago tradicional
+     * 1 – Preautorización
+     * O – Autorización en diferido
+     *
+     * @param mixed $transaction
+     * @throws \Exception
+     */
+    public function setTransactiontype($transaction = 'A')
+    {
+        if (strlen(trim($transaction)) > 0) {
+            $this->_parameters['DS_MERCHANT_TRANSACTIONTYPE'] = $transaction;
+        } else {
+            throw new \Exception('Please add transaction type');
+        }
+    }
+
+    /**
+     * (required)
+     * Número de terminal que le asignará su banco. Tres se considera su longitud máxima
+     *
+     * @param int $terminal
+     * @throws \Exception
+     */
+    public function setTerminal($terminal = 1)
+    {
+        if (intval($terminal) != 0) {
+            $this->_parameters['DS_MERCHANT_TERMINAL'] = $terminal;
+        } else {
+            throw new \Exception('Terminal is not valid.');
+        }
+    }
+
+    /**
+     * (required)
+     * Tarjeta. Su longitud depende del tipo de tarjeta.
+     *
+     * @param $pan
+     * @throws \Exception
+     */
+    public function setPan($pan)
+    {
+        if (intval($pan) != 0) {
+            $this->_parameters['DS_MERCHANT_PAN'] = $pan;
+        } else {
+            throw new \Exception('PAN is not valid.');
+        }
+    }
+
+    /**
+     * (required)
+     * Caducidad de la tarjeta.
+     * Su formato es AAMM, siendo AA los dos últimos dígitos del año y MM los dos dígitos del mes.
+     *
+     * @param $expirydate
+     * @throws \Exception
+     */
+    public function setExpiryDate($expirydate)
+    {
+        if (strlen(trim($expirydate)) == 4) {
+            $this->_parameters['DS_MERCHANT_EXPIRYDATE'] = $expirydate;
+        } else {
+            throw new \Exception('Expire date is not valid.');
+        }
+    }
+
+    /**
+     * (required)
+     * Código CVV2 de la tarjeta.
+     *
+     * @param $cvv
+     * @throws \Exception
+     */
+    public function setCVV($cvv)
+    {
+        if (intval($cvv) != 0) {
+            $this->_parameters['DS_MERCHANT_CVV2'] = $cvv;
+        } else {
+            throw new \Exception('CVV is not valid.');
+        }
+    }
+
+    /**
+     * @param bool $flat
+     * @throws \Exception
+     */
+    public function setMerchantDirectPayment($flat = false)
+    {
+        if (is_bool($flat)) {
+            $this->_parameters['DS_MERCHANT_DIRECTPAYMENT '] = $flat;
+        } else {
+            throw new \Exception('Please set true or false');
+        }
+    }
+
+
+    /**
+     * @param string $type
+     * @return array
+     */
+    public function firePayment($type = 'test')
+    {
+
+        $xml = $this->buildXML();
+        $client = new \SoapClient($this->getEnviroment($type));
+        $result = $client->trataPeticion(array('datoEntrada' => $xml));
+        $response = $this->xmltoArray($result->trataPeticionReturn);
+        return $this->checkResponse($response);
+    }
+
+
+    /**
+     * @param string $xml
+     * @return array
+     */
+    private function xmltoArray(string $xml)
+    {
+        $xml = simplexml_load_string($xml, "SimpleXMLElement", LIBXML_NOCDATA);
+        $json = json_encode($xml);
+        $response = json_decode($json, true);
+        return $response;
+    }
+
+    /**
+     *
+     * 0000 a 0099 Transacción autorizada para pagos y preautorizaciones
+     * 900 Transacción autorizada para devoluciones y confirmaciones
+     * 400 Transacción autorizada para anulaciones
+     *
+     * @param $response
+     * @return array
+     */
+    private function checkResponse($response)
+    {
+        $code = $response['CODIGO'];
+
+        if (!$this->validCode($code)) {
+            return $this->getResponse($code, $response['RECIBIDO']['REQUEST']['DATOSENTRADA'], true);
+        }
+
+        if (!$this->checkResponseSignature($response['OPERACION'])) {
+            return $this->getResponse('SIS041', $response['OPERACION'], true);
+        }
+
+        return $this->getResponse($code, $response['OPERACION']);
+    }
+
+    /**
+     * @param $postData
+     * @return array
+     * @throws \Exception
+     */
+    public function checkPaymentResponse($postData)
+    {
+        if (isset($postData)) {
+            $parameters = $postData["Ds_MerchantParameters"];
+            $signatureReceived = $postData["Ds_Signature"];
+            $decodec = json_decode($this->decodeParameters($parameters), true);
+            $order = $decodec['Ds_Order'];
+            $signature = $this->generateSignature($parameters, $order);
+            $signature = strtr($signature, '+/', '-_');
+            if ($signature === $signatureReceived) {
+                return $this->getResponse(0, $decodec);
+            } else {
+                return $this->getResponse('SIS041', $decodec, true);
+            }
+        } else {
+            throw new \Exception("Add data return of bank");
+        }
+    }
+
+    /**
+     * @param $code
+     * @param $response
+     * @param bool $error
+     * @return array
+     */
+    private function getResponse($code, $response, $error = false)
+    {
+        $response_default = [
+            'error' => $error,
+            'code'  => $code
+        ];
+        return array_merge($response_default, $response);
+    }
+
+    /**
+     * @param $response
+     * @return bool
+     */
+    private function checkResponseSignature($response)
+    {
+        $cadena = $response['Ds_Amount'] . $response['Ds_Order'] . $response['Ds_MerchantCode'] . $response['Ds_Currency'] .
+            $response['Ds_Response'] . $response['Ds_TransactionType'] . $response['Ds_SecurePayment'];
+
+        if ($this->generateSignature($cadena, $response['Ds_Order']) != $response['Ds_Signature']) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param $code
+     * @return bool
+     */
+    private function validCode($code)
+    {
+        if (!is_numeric($code)) {
+            return false;
+        }
+
+        if ($code >= 0 && $code < 100) {
+            return true;
+        }
+
+        if ($code == 900 || $code == 400) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $data
+     * @param $key
+     * @return string
+     */
+    private function hmac256($data, $key)
+    {
+        $sha256 = hash_hmac('sha256', $data, $key, true);
+        return $sha256;
+    }
+
+    /**
+     * @param $data
+     * @param $key
+     * @return string
+     */
+    private function encrypt_3DES($data, $key)
+    {
+        $iv = "\0\0\0\0\0\0\0\0";
+        $data_padded = $data;
+        if (strlen($data_padded) % 8) {
+            $data_padded = str_pad($data_padded, strlen($data_padded) + 8 - strlen($data_padded) % 8, "\0");
+        }
+        $ciphertext = openssl_encrypt($data_padded, "DES-EDE3-CBC", $key, OPENSSL_RAW_DATA | OPENSSL_NO_PADDING, $iv);
+        return $ciphertext;
+    }
+
+    /**
+     * @param $data
+     * @return string
+     */
+    private function encodeBase64($data)
+    {
+        $data = base64_encode($data);
+        return $data;
+    }
+
+    /**
+     * @param $data
+     * @return string
+     */
+    private function decodeBase64($data)
+    {
+        $data = base64_decode($data);
+        return $data;
+    }
+
+    /**
+     * @param $price
+     * @return string
+     */
+    private function convertNumber($price)
+    {
+        $number = number_format(str_replace(',', '.', $price), 2, '.', '');
+        return $number;
+    }
+
+    /**
+     * @param $enviroment
+     * @param string $type
+     * @return string
+     * @throws \Exception
+     */
+    private function getEnviroment($enviroment, $type = 'webservice')
+    {
+        if (trim($enviroment) == 'live') {
+            if ($type == 'webservice') {
+                $_enviroment = 'https://sis.redsys.es/sis/services/SerClsWSEntrada?wsdl';
+            } else {
+                $_enviroment = 'https://sis.redsys.es/sis/realizarPago';
+            }
+        } elseif (trim($enviroment) == 'test') {
+            if ($type == 'webservice') {
+                $_enviroment = 'https://sis-t.redsys.es:25443/sis/services/SerClsWSEntrada?wsdl';
+            } else {
+                $_enviroment = 'https://sis-t.redsys.es:25443/sis/realizarPago';
+            }
+
+        } else {
+            throw new \Exception('Add test or live');
+        }
+
+        return $_enviroment;
+
+    }
+
+    /**
+     * @return string
+     */
+    private function buildXML()
+    {
+        $datos = $this->getParameters();
+        $xml = '<REQUEST>';
+        $xml .= $datos;
+        $xml .= '<DS_SIGNATUREVERSION>HMAC_SHA256_V1</DS_SIGNATUREVERSION>';
+        $xml .= '<DS_SIGNATURE>' . $this->generateSignature($datos, $this->getOrder()) . '</DS_SIGNATURE>';
+        $xml .= '</REQUEST>';
+
+        return $xml;
+    }
+
+    /**
+     * @return string
+     */
+    private function getParameters()
+    {
+        $xml = '<DATOSENTRADA>';
+        foreach ($this->_parameters as $key => $value) {
+            $xml .= '<' . $key . '>' . $value . '</' . $key . '>';
+        }
+        $xml .= '</DATOSENTRADA>';
+        return $xml;
+    }
+
+    /**
+     * @param $datos
+     * @param $order
+     * @return string
+     */
+    public function generateSignature($datos, $order)
+    {
+        $key = $this->decodeBase64($this->key);
+        // Get key with Order and key
+        $key = $this->encrypt_3DES($order, $key);
+        // Generated Hmac256 of Merchant Parameter
+        $result = $this->hmac256($datos, $key);
+        // Base64 encoding
+        return $this->encodeBase64($result);
+    }
+
+    /**
+     * @param string $type
+     * @param array $options
+     * @return string
+     */
+    public function createForm($type = 'test', $options = [])
+    {
+
+        $default_options = [
+            'form_name' => 'formname',
+            'submit_value' => 'Pay'
+        ];
+
+        $options = array_merge($default_options, $options);
+
+        $environment = $this->getEnviroment($type, 'gateaway');
+        $form = '
+            <form action="' . $environment . '" method="post" name="'.$options['form_name'].'" >
+                <input type="hidden" name="Ds_MerchantParameters" value="' . $this->generateMerchantParameters() . '"/>
+                <input type="hidden" name="Ds_Signature" value="' . $this->generateSignature($this->generateMerchantParameters(),
+                $this->getOrder()) . '"/>
+                <input type="hidden" name="Ds_SignatureVersion" value="HMAC_SHA256_V1"/>
+                <input type="submit" name="submitname" value="'.$options['submit_value'].'">
+            </form>
+        ';
+        return $form;
+    }
+
+    /**
+     * @return string
+     */
+    private function generateMerchantParameters()
+    {
+        $json = json_encode($this->_parameters);
+        return $this->encodeBase64($json);
+    }
+
+    /**
+     * @param $data
+     * @return string
+     */
+    private function decodeParameters($data)
+    {
+        $decode = base64_decode(strtr($data, '-_', '+/'));
+        return $decode;
+    }
+}
